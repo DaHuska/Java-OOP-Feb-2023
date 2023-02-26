3cfaa520fc410d72ffa420303b1348681b9ba808
Alex Boyanov <aboyanov@abv.bg>
2023-02-26 19:58:24 +0200

    Discount enum

---
+++ Working with Abstraction/_04_HotelReservation/Discount.java
@@ -0,0 +1,17 @@
+package com.company._04_HotelReservation;
+
+public enum Discount {
+    VIP(0.2),
+    SecondVisit(0.1),
+    None(.0);
+
+    private double discount;
+
+    Discount(Double discount) {
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+}
