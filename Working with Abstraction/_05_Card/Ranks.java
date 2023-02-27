14bbd58e87afb349f696e01ec6010eb1b45249e8
Alex Boyanov <aboyanov@abv.bg>
2023-02-27 22:41:20 +0200

    Ranks file

---
+++ Working with Abstraction/_05_Card/Ranks.java
@@ -0,0 +1,17 @@
+package com.company._05_Card;
+
+public enum Ranks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+}
