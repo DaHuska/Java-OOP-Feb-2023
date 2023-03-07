8e3e093fea2bdfd4bff7eb5c76e6001b394fea91
Alex Boyanov <aboyanov@abv.bg>
2023-03-07 21:03:16 +0200

    Pizza class

---
+++ Encapsulation/_06_PizzaCalories/Pizza.java
@@ -0,0 +1,60 @@
+package com.company.Encapsulation._06_PizzaCalories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza {
+    private String name;
+    private Dough dough;
+    private List<Topping> toppings;
+
+    public Pizza(String name, int numberOfToppings) {
+        setName(name);
+        setToppings(numberOfToppings);
+    }
+
+    private void setToppings(int numberOfToppings) {
+        if (!Validations.validateToppingsNumber(numberOfToppings)) {
+            throw new IllegalArgumentException("Number of toppings should be in range [0..10].");
+        }
+
+        this.toppings = new ArrayList<>(numberOfToppings);
+    }
+
+    private void setName(String name) {
+        if (!Validations.validatePizzaName(name)) {
+            throw new IllegalArgumentException("Pizza name should be between 1 and 15 symbols.");
+        }
+
+        this.name = name;
+    }
+
+    public void setDough(Dough dough) {
+        this.dough = dough;
+    }
+
+    public void addTopping(Topping topping) {
+        // TODO: set toppings to not accept more than its capacity
+        this.toppings.add(topping);
+    }
+
+    public double getOverallCalories() {
+        return calculateOverallCalories();
+    }
+
+    private double calculateOverallCalories() {
+        double overallCalories = 0;
+
+        overallCalories += dough.calculateCalories();
+
+        for (Topping topping : this.toppings) {
+            overallCalories += topping.calculateCalories();
+        }
+
+        return overallCalories;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
