72f460c521937b9d3ba3079cf963e79a06cd30f0
Alex Boyanov <aboyanov@abv.bg>
2023-03-07 21:02:48 +0200

    Dough class

---
+++ Encapsulation/_06_PizzaCalories/Dough.java
@@ -0,0 +1,65 @@
+package com.company.Encapsulation._06_PizzaCalories;
+
+public class Dough {
+    private String flourType;
+    private String bakingTechnique;
+    private double weight;
+
+    public Dough(String flourType, String bakingTechnique, double weight) {
+        setFlourType(flourType);
+        setBakingTechnique(bakingTechnique);
+        setWeight(weight);
+    }
+
+    public double calculateCalories() {
+        double flourModifier = 0;
+        double techniqueModifier = 0;
+
+        switch (this.flourType) {
+            case "White":
+                flourModifier = 1.5;
+                break;
+            case "Wholegrain":
+                flourModifier = 1.;
+                break;
+        }
+
+        switch (this.bakingTechnique) {
+            case "Crispy":
+                techniqueModifier = 0.9;
+                break;
+            case "Chewy":
+                techniqueModifier = 1.1;
+                break;
+            case "Homemade":
+                techniqueModifier = 1.;
+                break;
+        }
+
+        return (2 * this.weight) * flourModifier * techniqueModifier;
+    }
+
+    public void setFlourType(String flourType) {
+        if (!Validations.validateFlour(flourType)) {
+            throw new IllegalArgumentException("Invalid type of dough.");
+        }
+
+        this.flourType = flourType;
+    }
+
+    public void setBakingTechnique(String bakingTechnique) {
+        if (!Validations.validateTechnique(bakingTechnique)) {
+            throw new IllegalArgumentException("Invalid type of dough.");
+        }
+
+        this.bakingTechnique = bakingTechnique;
+    }
+
+    public void setWeight(double weight) {
+        if (!Validations.validateDoughWeight(weight)) {
+            throw new IllegalArgumentException("Dough weight should be in the range [1..200].");
+        }
+
+        this.weight = weight;
+    }
+}
