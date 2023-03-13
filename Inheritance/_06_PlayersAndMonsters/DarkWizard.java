14e1df7a67a982c5974f4c3e8a5af9abed713d13
Alex Boyanov <aboyanov@abv.bg>
2023-03-13 23:38:13 +0200

    Dark Wizard class

---
+++ Inheritance/_06_PlayersAndMonsters/DarkWizard.java
@@ -0,0 +1,7 @@
+package com.company.Inheritance._06_PlayersAndMonsters;
+
+public class DarkWizard extends Wizard {
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
