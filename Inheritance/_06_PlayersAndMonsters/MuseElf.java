a7f268916905fa95c80df96c0fb9c54267747de7
Alex Boyanov <aboyanov@abv.bg>
2023-03-13 23:38:59 +0200

    Muse Elf

---
+++ Inheritance/_06_PlayersAndMonsters/MuseElf.java
@@ -0,0 +1,7 @@
+package com.company.Inheritance._06_PlayersAndMonsters;
+
+public class MuseElf extends Elf {
+    public MuseElf(String username, int level) {
+        super(username, level);
+    }
+}
