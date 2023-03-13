acc2f99f483ea41a09b9d1f80a9a31c03f96314e
Alex Boyanov <aboyanov@abv.bg>
2023-03-13 23:38:46 +0200

    Knight class

---
+++ Inheritance/_06_PlayersAndMonsters/Knight.java
@@ -0,0 +1,7 @@
+package com.company.Inheritance._06_PlayersAndMonsters;
+
+public class Knight extends Hero {
+    public Knight(String username, int level) {
+        super(username, level);
+    }
+}
