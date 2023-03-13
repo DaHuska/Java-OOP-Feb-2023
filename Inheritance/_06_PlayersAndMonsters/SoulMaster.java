c46927d889eb91446702bc68aeb40bc01a496d80
Alex Boyanov <aboyanov@abv.bg>
2023-03-13 23:39:13 +0200

    Soul Master class

---
+++ Inheritance/_06_PlayersAndMonsters/SoulMaster.java
@@ -0,0 +1,7 @@
+package com.company.Inheritance._06_PlayersAndMonsters;
+
+public class SoulMaster extends DarkWizard {
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
