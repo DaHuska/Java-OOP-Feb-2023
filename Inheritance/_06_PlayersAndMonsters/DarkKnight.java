30f38ac2430b74c62bbdb1311fa161673a041e79
Alex Boyanov <aboyanov@abv.bg>
2023-03-13 23:37:58 +0200

    Dark Knight class

---
+++ Inheritance/_06_PlayersAndMonsters/DarkKnight.java
@@ -0,0 +1,7 @@
+package com.company.Inheritance._06_PlayersAndMonsters;
+
+public class DarkKnight extends Knight {
+    public DarkKnight(String username, int level) {
+        super(username, level);
+    }
+}
