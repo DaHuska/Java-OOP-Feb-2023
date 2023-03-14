8dd17fca416afd94369b9eb3f374d3d676c84d39
Alex Boyanov <aboyanov@abv.bg>
2023-03-14 23:25:16 +0200

    Motorcycle class

---
+++ Inheritance/_07_NeedForSpeed/Motorcycle.java
@@ -0,0 +1,7 @@
+package com.company.Inheritance._07_NeedForSpeed;
+
+public class Motorcycle extends Vehicle {
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
