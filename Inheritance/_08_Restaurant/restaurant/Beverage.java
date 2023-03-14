a24b4a0e4ec8f9d2537a9852c56980e2b1920ae8
Alex Boyanov <aboyanov@abv.bg>
2023-03-14 23:26:52 +0200

    Beverage class

---
+++ Inheritance/_08_Restaurant/restaurant/Beverage.java
@@ -0,0 +1,16 @@
+package com.company.Inheritance._08_Restaurant.restaurant;
+
+import java.math.BigDecimal;
+
+public class Beverage extends Product {
+    private double milliliters;
+
+    public Beverage(String name, BigDecimal price, double milliliters) {
+        super(name, price);
+        this.milliliters = milliliters;
+    }
+
+    public double getMilliliters() {
+        return milliliters;
+    }
+}
