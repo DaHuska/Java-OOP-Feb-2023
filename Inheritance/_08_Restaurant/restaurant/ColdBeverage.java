613213b5fcb50a18ff67933a6f0134fee8fc7eb4
Alex Boyanov <aboyanov@abv.bg>
2023-03-14 23:27:22 +0200

    Cold Beverage class

---
+++ Inheritance/_08_Restaurant/restaurant/ColdBeverage.java
@@ -0,0 +1,9 @@
+package com.company.Inheritance._08_Restaurant.restaurant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage {
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
