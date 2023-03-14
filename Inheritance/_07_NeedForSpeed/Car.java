d8d24b71d32603b3abf1671847a71a05ca623087
Alex Boyanov <aboyanov@abv.bg>
2023-03-14 23:24:23 +0200

    Car class

---
+++ Inheritance/_07_NeedForSpeed/Car.java
@@ -0,0 +1,10 @@
+package com.company.Inheritance._07_NeedForSpeed;
+
+public class Car extends Vehicle {
+    private static final double DEFAULT_FUEL_CONSUMPTION = 3;
+
+    public Car(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
