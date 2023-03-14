15cf4cc688b82ff3d0984c0b431a23ff83d2cf2a
Alex Boyanov <aboyanov@abv.bg>
2023-03-14 23:24:43 +0200

    Cross Motorcycle class

---
+++ Inheritance/_07_NeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package com.company.Inheritance._07_NeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle {
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
