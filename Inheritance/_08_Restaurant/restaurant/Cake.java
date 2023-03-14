1a09919e265a70f1e29171d12ea03d7ad38998e2
Alex Boyanov <aboyanov@abv.bg>
2023-03-14 23:27:00 +0200

    Cake class

---
+++ Inheritance/_08_Restaurant/restaurant/Cake.java
@@ -0,0 +1,13 @@
+package com.company.Inheritance._08_Restaurant.restaurant;
+
+import java.math.BigDecimal;
+
+public class Cake extends Dessert {
+    private static final double CAKE_GRAMS = 250;
+    private static final double CAKE_CALORIES = 1000;
+    private static final BigDecimal CAKE_PRICE = BigDecimal.valueOf(5);
+
+    public Cake(String name) {
+        super(name, CAKE_PRICE, CAKE_GRAMS, CAKE_CALORIES);
+    }
+}
