a145ecf7d9604f62771a8c2dcb4aa9616aeeb571
Alex Boyanov <aboyanov@abv.bg>
2023-03-14 23:28:04 +0200

    Hot Beverage class

---
+++ Inheritance/_08_Restaurant/restaurant/HotBeverage.java
@@ -0,0 +1,9 @@
+package com.company.Inheritance._08_Restaurant.restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverage {
+    public HotBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
