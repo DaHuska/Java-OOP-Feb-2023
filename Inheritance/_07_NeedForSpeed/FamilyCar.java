e9dff531086185b4c86a1cdee35c023ccae59539
Alex Boyanov <aboyanov@abv.bg>
2023-03-14 23:25:00 +0200

    Family Car class

---
+++ Inheritance/_07_NeedForSpeed/FamilyCar.java
@@ -0,0 +1,7 @@
+package com.company.Inheritance._07_NeedForSpeed;
+
+public class FamilyCar extends Car {
+    public FamilyCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
