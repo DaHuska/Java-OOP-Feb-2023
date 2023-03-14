dd19e072b1a2a886ec1a2bd08c5e4715d7b1baf5
Alex Boyanov <aboyanov@abv.bg>
2023-03-14 23:25:31 +0200

    Race Motorcycle class

---
+++ Inheritance/_07_NeedForSpeed/RaceMotorcycle.java
@@ -0,0 +1,10 @@
+package com.company.Inheritance._07_NeedForSpeed;
+
+public class RaceMotorcycle extends Motorcycle {
+    private static final double DEFAULT_FUEL_CONSUMPTION = 8;
+
+    public RaceMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
