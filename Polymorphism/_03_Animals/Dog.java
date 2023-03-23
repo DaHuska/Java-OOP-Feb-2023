dc94f317727b877fb3f8936ad0b39e97ac5c108f
Alex Boyanov <aboyanov@abv.bg>
2023-03-23 23:16:47 +0200

    Dog class

---
+++ Polymorphism/_03_Animals/Dog.java
@@ -0,0 +1,12 @@
+package com.company.Polymorphism._03_Animals;
+
+public class Dog extends Animal {
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%nDJAAF", getName(), getFavouriteFood());
+    }
+}
