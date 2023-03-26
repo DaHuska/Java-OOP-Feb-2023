dcc74c86bc74aaf669e1be7bf7c71582bc3cab25
Alex Boyanov <aboyanov@abv.bg>
2023-03-27 00:02:17 +0300

    Vegetable class

---
+++ Polymorphism/_04_WildFarm/Vegetable.java
@@ -0,0 +1,8 @@
+package com.company.Polymorphism._04_WildFarm;
+
+public class Vegetable extends Food {
+    public Vegetable(int quantity) {
+        super(quantity);
+        setFoodType("Vegetable");
+    }
+}
