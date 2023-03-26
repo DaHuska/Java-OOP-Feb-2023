e5aba84d954fbf86ad67848092897dcd5068f7df
Alex Boyanov <aboyanov@abv.bg>
2023-03-27 00:00:50 +0300

    Felime abstract class

---
+++ Polymorphism/_04_WildFarm/Felime.java
@@ -0,0 +1,7 @@
+package com.company.Polymorphism._04_WildFarm;
+
+public abstract class Felime extends Mammal {
+    public Felime(String type, String name, double weight, String livingRegion) {
+        super(type, name, weight, livingRegion);
+    }
+}
