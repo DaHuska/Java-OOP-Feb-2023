889a79fdea81aca9cf2bbdebb34d0375dde9a369
Alex Boyanov <aboyanov@abv.bg>
2023-03-27 00:01:30 +0300

    Meat class

---
+++ Polymorphism/_04_WildFarm/Meat.java
@@ -0,0 +1,8 @@
+package com.company.Polymorphism._04_WildFarm;
+
+public class Meat extends Food {
+    protected Meat(int quantity) {
+        super(quantity);
+        setFoodType("Meat");
+    }
+}
