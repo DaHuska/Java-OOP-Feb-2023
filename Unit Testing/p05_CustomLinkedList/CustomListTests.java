c4c9772eac3542d5e58af271d83bd8d1c5f3eb2d
Alex Boyanov <aboyanov@abv.bg>
2023-04-24 23:27:49 +0300

    Custom Linked List Tests

--- Unit Testing/p05_CustomLinkedList/CustomListTests.java
+++ Unit Testing/p05_CustomLinkedList/CustomListTests.java
@@ -1,9 +1,14 @@
 package p05_CustomLinkedList;
 
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CustomListTests {
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
     @Test
     public void testCustomLinkedListAdd() {
@@ -42,5 +47,25 @@ public class CustomListTests {
         Assert.assertFalse(customLinkedList.contains("Tarator"));
     }
 
-    
+    @Test
+    public void testCustomLinkedListSetWhenInvalidIndex() {
+        exceptionRule.expect(IllegalArgumentException.class);
+
+        CustomLinkedList<String> customLinkedList = new CustomLinkedList<>();
+
+        customLinkedList.add("Banica");
+        customLinkedList.set(2, "Tarator");
+    }
+
+    @Test
+    public void testCustomLinkedListSetWhenValidIndex() {
+        CustomLinkedList<String> customLinkedList = new CustomLinkedList<>();
+
+        customLinkedList.add("Banica");
+        customLinkedList.set(0, "Tarator");
+
+        String item = customLinkedList.get(0);
+
+        Assert.assertEquals("Tarator", item);
+    }
 }
